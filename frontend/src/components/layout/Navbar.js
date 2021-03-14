3e436e66886c9cc2aa10e37c59cc38cc98f452ce
Abhishek Ranjan <ranjan.abhishek.kotn@gmail.com>
2021-03-14 23:58:30 +0530

    Redux Store and Alert Actions implmented

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,17 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className='navbar bg-dark'>
       <h1>
-        <a href='index.html'>SimplySocial</a>
+        <Link to='/'>SimplySocial</Link>
       </h1>
       <ul>
         <li>
-          <a href='register.html'>Register</a>
+          <Link to='/register'>Register</Link>
         </li>
         <li>
-          <a href='login.html'>Login</a>
+          <Link to='/login'>Login</Link>
         </li>
       </ul>
     </nav>
